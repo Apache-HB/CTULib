1961569255333a862c0478e7971ef7e455a84ffa
Elliot Haisley <apachehaisley@gmail.com>
2018-11-02 17:40:57 -0400

    fix the build files not working

--- Build/Scripts/MakeLibrary.sh
+++ Build/Scripts/MakeLibrary.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-mv *.o Build/Binaries/Objects/Cthulhu/
-
 lib_path="Build/Binaries/Libraries/Cthulhu/Cthulhu.a"
 
-if [ -a $lib_path ]; then
-    rm -rf "$lib_path"
-fi
+mkdir -p Build/Binaries/Objects/Cthulhu
+mkdir -p Build/Binaries/Libraries/Cthulhu
+
+mv *.o Build/Binaries/Objects/Cthulhu/
+
+rm -rf "$lib_path"
 
 ar rcs $lib_path Build/Binaries/Objects/Cthulhu/*.o
